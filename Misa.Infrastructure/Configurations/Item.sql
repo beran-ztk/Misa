6edefb2926352ef21d0e3d32ae40270bc3135863
Beran Öztürk <beran-ozturk2002@outlook.de>
2025-12-03 15:17:03 +0100

    Item und Item-RS Tabelle angepasst und verbessert

--- Misa.Infrastructure/Configurations/Item.sql
+++ Misa.Infrastructure/Configurations/Item.sql
@@ -1,35 +1,51 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto"; -- For gen_random_uuid() function
-
--- create table items
--- (
-    -- id            int generated always as identity (start with 100000) primary key,
-    -- fk_state      int references lu_states(id) on delete restrict,
-    -- fk_priority   int references lu_priorities(id) on delete restrict,
-    -- fk_category   int references lu_categories(id) on delete restrict,
-    -- name          text not null,
-    -- note          text,
-    -- ts_created    timestamptz default now()
--- );
-
+       
 CREATE TABLE items
 (
-    id              UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id              UUID DEFAULT   gen_random_uuid() PRIMARY KEY,
+    
+    owner_id        UUID,
+    state_id        INT REFERENCES lu_states(id)     ON DELETE RESTRICT,
+    priority_id     INT REFERENCES lu_priorities(id) ON DELETE RESTRICT,
+    category_id     INT REFERENCES lu_categories(id) ON DELETE RESTRICT,
+    
     title           TEXT NOT NULL,
     description     TEXT,
-    created_at_utc  TIMESTAMPTZ DEFAULT now()
-);
-
-
-create table item_relations
-(
-    id              int generated always as identity primary key,
-    fk_item_parent  int references items(id) on delete restrict,
-    fk_item_child   int references items(id) on delete restrict,
-    fk_relation     int references lu_relations(id) on delete restrict,
     
-    constraint unique_link unique (fk_item_parent, fk_item_child, fk_relation),
-    constraint no_self_link check (fk_item_parent != fk_item_child)
+    created_at_utc  TIMESTAMPTZ DEFAULT now(),
+    updated_at_utc  TIMESTAMPTZ,
 );
+CREATE INDEX idx_items_owner
+    ON items(owner_id);
+CREATE INDEX idx_items_state
+    ON items(state_id);
+CREATE INDEX idx_items_priority
+    ON items(priority_id);
+CREATE INDEX idx_items_category
+    ON items(category_id);
+CREATE INDEX idx_items_created
+    ON items(created_at_utc);
+
+
+CREATE TABLE item_relations
+(
+    id              INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    item_parent_id  UUID REFERENCES items(id) ON DELETE RESTRICT,
+    item_child_id   UUID REFERENCES items(id) ON DELETE RESTRICT,
+    relation_id     INT  REFERENCES lu_relations(id) ON DELETE RESTRICT,
+
+    created_at_utc TIMESTAMPTZ DEFAULT now()
+
+    CONSTRAINT no_self_link CHECK (item_parent_id != item_child_id),
+    CONSTRAINT unique_link UNIQUE (item_parent_id, item_child_id, relation_id)
+);
+CREATE INDEX idx_item_relations_parent 
+    ON item_relations(item_parent_id);
+CREATE INDEX idx_item_relations_child
+    ON item_relations(item_child_id);
+CREATE INDEX idx_item_relations_relation
+    ON item_relations(relation_id);
 
 create table item_modules
 (
